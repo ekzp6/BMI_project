f25e1b0ec722509d6c861a78c68a5f8fc5297fa8
ekzp6 <eaking@wustl.edu>
2020-11-05 17:24:38 -0600

    Updated R file to reflect normalized IgG and IgM values.

--- R_files/processing.R
+++ R_files/processing.R
@@ -1,5 +1,11 @@
 library(tidyverse)
+library(affyPLM)
+library(gcrma)
+library(preprocessCore)
 
+setwd("~/FS20/BMI_intro/project/BMI_project/R_files")
+
+### Data acquisition and formating 
 annotation <- read_delim('C:/Users/ylime/Documents/FS20/BMI_intro/project/BMI_project/sample_annotation.txt',delim = "\t")
 samples1 <- filter(annotation, annotation$File == 1)
 samples2 <- filter(annotation, annotation$File == 2)
@@ -16,4 +22,22 @@ data2 <-  merge(data2,samples2,by="Block")
 data3 <-  merge(data3,samples3,by="Block")
 data4 <-  merge(data4,samples4,by="Block")
 
-merged_data <- rbind(data1,data2,data3,data4)
+merged_data <- as_tibble(rbind(data1,data2,data3,data4))
+lapply(merged_data,class)
+
+merged_IgG <- as_tibble(merged_data[,c('Samples ID','F532 Median - B532')])
+merged_IgM <- as_tibble(merged_data[,c('Samples ID','F635 Median - B635')])
+
+write_delim(merged_data,'~/FS20/BMI_intro/project/BMI_project/merged_data/merged_data.txt',delim="\t")
+write_delim(merged_IgG,'~/FS20/BMI_intro/project/BMI_project/merged_data/merged_IgG.txt',delim = "\t")
+write_delim(merged_IgM,'~/FS20/BMI_intro/project/BMI_project/merged_data/merged_IgM.txt',delim = "\t")
+
+### Visualizing Quality Control of Normalized Data
+boxplot(`F532 Median - B532`~`Samples ID`,merged_data,
+        main = "Normalized IgG binding")
+boxplot(`F635 Median - B635`~`Samples ID`,merged_data,
+        main = "Normalized IgM binding")
+
+# put histogram here
+
+
